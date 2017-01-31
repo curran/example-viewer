43bbfd1d98a47a52b1f336e858b729e989d098ca
curran <curran.kelleher@gmail.com>
2017-01-31 21:34:13 +0530

    Got basic save middleware working

--- index.js
+++ index.js
@@ -4,9 +4,16 @@
 // inside the presentation environment.
 
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
+app.use(bodyParser.json());
 app.use(express.static("."));
+
+app.post("/save", function (req, res){
+  console.log(req.body);
+  res.send("ok");
+});
 
 app.listen(3000, function () {
   console.log("Listening at http://localhost:3000");
