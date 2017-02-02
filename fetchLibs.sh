6e87e194cdc50581a69a6bf7a057b302811cd345
curran <curran.kelleher@gmail.com>
2017-02-02 19:54:03 +0530

    Adopt Inlet

--- fetchLibs.sh
+++ fetchLibs.sh
@@ -1,10 +1,15 @@
 # For offline development
 mkdir libs
 cd libs
+
 wget https://cdnjs.cloudflare.com/ajax/libs/d3/4.5.0/d3.min.js
+
 wget https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.23.0/codemirror.min.js
 wget https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.23.0/mode/javascript/javascript.min.js
 wget https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.23.0/mode/css/css.min.js
 wget https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.23.0/mode/xml/xml.min.js
 wget https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.23.0/mode/htmlmixed/htmlmixed.min.js
 wget https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.23.0/codemirror.min.css
+
+wget https://enjalot.github.io/Inlet/inlet.min.js
+wget https://enjalot.github.io/Inlet/inlet.css
