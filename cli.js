d2276322b44b06ac8a6a6d0894c2d38aaf155754
curran <curran.kelleher@gmail.com>
2017-02-03 19:52:22 +0530

    Modify CLI to copy index.html

--- cli.js
+++ cli.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
+
+// Copy over the index.html
+var fs = require("fs");
+var path = require("path");
+fs.createReadStream(path.join(__dirname, "index.html"))
+  .pipe(fs.createWriteStream("index.html"));
+
+// Launch the server.
 require("./index.js");
