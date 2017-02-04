520ace5e0c00cac4e90e218ff53543eb4b5baf1b
curran <curran.kelleher@gmail.com>
2017-02-04 15:34:16 +0530

    Adopt redux and redux-thunk

--- fetchLibs.sh
+++ fetchLibs.sh
@@ -13,3 +13,6 @@ wget https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.23.0/codemirror.min.css
 
 wget https://enjalot.github.io/Inlet/inlet.min.js
 wget https://enjalot.github.io/Inlet/inlet.css
+
+wget https://cdnjs.cloudflare.com/ajax/libs/redux/3.6.0/redux.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/redux-thunk/2.2.0/redux-thunk.min.js
