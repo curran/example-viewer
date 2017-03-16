545317550a3cdf7a1a453c700d8fff4cda824ab2
curran <curran.kelleher@gmail.com>
2017-03-16 17:21:57 +0530

    Prepare files for runner

--- src/client/getFiles.js
+++ src/client/getFiles.js
@@ -10,13 +10,22 @@ export function getFiles({ index, params }){
   ]);
 };
 
+// Gets the listing of file entries for the current example,
+// but returns an empty object if not all of the files have loaded yet.
+export function getLoadedFiles(state){
+  const files = getFiles(state);
+  const loaded = files && Object.keys(files)
+    .every(function (name){
+      return files[name]; // The value is "false" if it hasn't loaded yet.
+    });
+  return loaded ? files : undefined;
+}
+
 // Gets the listing of files as an array of objects.
 export function getFilesSorted(state){
-  const filesDictionary = getFiles(state);
-  return (filesDictionary ? Object.keys(filesDictionary) : [])
-    .map((name) => ({
-      name,
-      content: filesDictionary[name]
-    }))
-    .filter((d) => d.content);
+  const files = getLoadedFiles(state);
+  return files ? Object.keys(files).map((name) => ({
+    name,
+    content: files[name]
+  })) : [];
 };
