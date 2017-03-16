1ce2d99c31378dc562ebb6a0275141f76ce5b85b
curran <curran.kelleher@gmail.com>
2017-03-16 16:35:28 +0530

    Restore codemirror editor

--- src/client/editor.js
+++ src/client/editor.js
@@ -1,12 +1,32 @@
 import { component } from "d3-component";
 import { select, local } from "d3-selection";
 
-const codemirror = local();
+const codeMirrorLocal = local();
 
 // User interface component for the code editor.
-export default component("pre")
-  .render(function ({ content, onChange}){
-    d3.select(this).text(content);
+export default component("div")
+  .create(function ({ onChange }){
+    const codeMirror = codeMirrorLocal
+      .set(this, CodeMirror(this, {
+        lineNumbers: false,
+        mode: "htmlmixed"
+      }));
+
+    // Inlet provides the interactive sliders and color pickers.
+    Inlet(codeMirror);
+
+    codeMirror.on("change", function (editor, change){
+      if(change.origin === "setValue") return;
+      onChange(cm.getValue());
+      //dispatch(actions.changeHtml(cm.getValue()));
+    });
+  })
+  .render(function ({ content }){
+    console.log(content);
+    const codeMirror = codeMirrorLocal.get(this);
+    if(codeMirror.getValue() !== content){ // TODO use timestamp here?
+      codeMirror.setValue(content);
+    }
   });
 
 //export default function (dispatch, actions){
@@ -16,28 +36,10 @@ export default component("pre")
 //      editor.enter().append("div")
 //        .attr("class", "editor shadow")
 //        .each(function (){
-//          var cm = CodeMirror(this, {
-//            lineNumbers: false,
-//            mode: "htmlmixed"
-//          });
-//
-//          // Inlet provides the interactive sliders and color pickers.
-//          Inlet(cm);
-//
-//          cm.on("change", function (editor, change){
-//            if(change.origin === "setValue") return;
-//            dispatch(actions.changeHtml(cm.getValue()));
-//          });
-//
-//          codemirror.set(this, cm);
 //        }));
 //
 //    if(state.html){
 //      editor.each(function (){
-//        var cm = codemirror.get(this);
-//        if(cm.getValue() !== state.html){
-//          cm.setValue(state.html);
-//        }
 //      });
 //    }
 //  };
