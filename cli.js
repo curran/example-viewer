9bae0dce23059a63f444181a945f010028f73e5c
curran <curran.kelleher@gmail.com>
2017-03-17 15:58:10 +0530

    Fix cli, try to include dist in package.json

--- cli.js
+++ cli.js
@@ -9,11 +9,11 @@ fs.createReadStream(path.join(__dirname, "index.html"))
   .pipe(fs.createWriteStream("index.html"));
 
 mkdirp("example-viewer-dist", function (err) {
-  fs.createReadStream(path.join(__dirname, "bundle.min.js"))
-    .pipe(fs.createWriteStream(path.join("example-viewer-dist", "index.html"));
-  fs.createReadStream(path.join(__dirname, "styles.css"))
-    .pipe(fs.createWriteStream(path.join("example-viewer-dist", "styles.css"));
+  fs.createReadStream(path.join(__dirname, "example-viewer-dist", "bundle.min.js"))
+    .pipe(fs.createWriteStream(path.join("example-viewer-dist", "index.html")));
+  fs.createReadStream(path.join(__dirname, "example-viewer-dist", "styles.css"))
+    .pipe(fs.createWriteStream(path.join("example-viewer-dist", "styles.css")));
 });
 
 // Launch the server.
-require("./index.js");
+require("./src/server/index.js");
