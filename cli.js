b9ceeefe8c6473b091f0148ac0e837ae33aacf62
curran <curran.kelleher@gmail.com>
2017-03-30 11:27:37 +0530

    Use CDN

--- cli.js
+++ cli.js
@@ -1,19 +1,11 @@
 #!/usr/bin/env node
 
-// Copy over the index.html
 var fs = require("fs");
 var path = require("path");
-var mkdirp = require("mkdirp");
 
+// Copy over the index.html into the project that uses example-viewer.
 fs.createReadStream(path.join(__dirname, "index.html"))
   .pipe(fs.createWriteStream("index.html"));
-
-mkdirp("example-viewer-dist", function (err) {
-  fs.createReadStream(path.join(__dirname, "example-viewer-dist", "bundle.min.js"))
-    .pipe(fs.createWriteStream(path.join("example-viewer-dist", "bundle.min.js")));
-  fs.createReadStream(path.join(__dirname, "example-viewer-dist", "styles.css"))
-    .pipe(fs.createWriteStream(path.join("example-viewer-dist", "styles.css")));
-});
 
 // Launch the server.
 require("./src/server/index.js");
